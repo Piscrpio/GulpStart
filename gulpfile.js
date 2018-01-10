58cd47b1573dbe8abc0e80c2ca963ffaa6f9d439
houyazhao <1048997225@qq.com>
2018-01-10 16:53:52 +0800

    gulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
+var babel = require("gulp-babel");
+var autoprefixer = require('gulp-autoprefixer');
 
 // Basic Gulp task syntax
 // gulp.task('hello', function() {
@@ -30,10 +32,23 @@ gulp.task('browserSync', function() {
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
         .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'Android >= 4.0'],
+            cascade: true, //是否美化属性值 默认：true 像这样：
+            remove: true //是否去掉不必要的前缀 默认：true
+        }))
         .pipe(gulp.dest('app/css')) // Outputs it in the css folder
         .pipe(browserSync.reload({ // Reloading with Browser Sync
             stream: true
         }));
+})
+gulp.task('convertJS', function() {
+    return gulp.src('app/js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
 })
 
 // Watchers
@@ -48,10 +63,8 @@ gulp.task('watch', function() {
 
 // Optimizing CSS and JavaScript 
 gulp.task('useref', function() {
-
     return gulp.src('app/*.html')
         .pipe(useref())
-        .pipe(gulpIf('*.js', uglify()))
         .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('dist'));
 });
@@ -96,7 +109,7 @@ gulp.task('default', function(callback) {
 gulp.task('build', function(callback) {
     runSequence(
         'clean:dist',
-        'sass', ['useref', 'images', 'fonts'],
+        'sass', ['useref', 'convertJS', 'images', 'fonts'],
         callback
     )
 })
